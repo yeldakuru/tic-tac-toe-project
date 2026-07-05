0adc88bfacd6d0d25c58a6ecc602fbd58f723a9d
Yelda Kuru <yeldakuru99@outlook.com>
2026-07-05 17:19:56 +0300

    added gameboard.jsx and player.jsx

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+
+        //setIsEditing(!isEditing); bu yavaş yöntemdir. Çünkü önceki state değerine bağlı olarak yeni state değerini belirler. Bu nedenle, önceki state değerini kullanmak için bir fonksiyon kullanmak daha güvenlidir.
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(e) { //e ya da event yaygın olarak kullanılır
+        setPlayerName(e.target.value); // two way binding : input value değiştiğinde state güncellenir ve state değiştiğinde input value güncellenir. Bu sayede input value ve state her zaman senkronize olur.
+    }
+
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
+
+    }
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+
+                <span className="player-symbol">{symbol}</span>
+
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    )
+
+
+
+}
