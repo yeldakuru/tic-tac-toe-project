1a565e5e88dc077051392028d9b710a31a24b6f2
Yelda Kuru <yeldakuru99@outlook.com>
2026-07-05 22:29:17 +0300

    gameboard updated and log.jsx added

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,38 @@
 
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];//yeni bir dizi oluşturuyor. innerarray ile satırları da kopyalıyoruz.
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol; //yeni dizinin ilgili hücresine aktif oyuncunun sembolünü atıyor.
+            return updatedBoard;//yeni diziyi döndürüyor
+        });
+
+        onSelectSquare();
+
+
+    }
 
     return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex} >
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex} >
 
             <ol>
-                {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
+                {row.map((playerSymbol, colIndex) => <li key={colIndex}>
+                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                        {playerSymbol}
+                    </button>
+                </li>)}
             </ol>
 
         </li>)}
